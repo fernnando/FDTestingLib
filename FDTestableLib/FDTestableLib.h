0ffa23735a0047958e38b7d8968f10d264a4d38d
Fernnando <frontend@mycatwalk.com>
2021-08-11 17:44:45 -0300

    Initial Commit

---
+++ FDTestableLib/FDTestableLib.h
@@ -0,0 +1,19 @@
+//
+//  FDTestableLib.h
+//  FDTestableLib
+//
+//  Created by CTWLK on 8/11/21.
+//  Copyright © 2021 CATWALK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FDTestableLib.
+FOUNDATION_EXPORT double FDTestableLibVersionNumber;
+
+//! Project version string for FDTestableLib.
+FOUNDATION_EXPORT const unsigned char FDTestableLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDTestableLib/PublicHeader.h>
+
+
