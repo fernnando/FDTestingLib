faff573d0460c93cac5ac695e2cc33b1cf48a47c
Fernnando <frontend@mycatwalk.com>
2021-08-11 17:51:24 -0300

    Initial project setup

---
+++ FDTestableLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FDTestableLibExamples
+//
+//  Created by CTWLK on 8/11/21.
+//  Copyright © 2021 CATWALK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
